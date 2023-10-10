2984be1be487a5f7ec0845548db104d67394e856
Gouvernon <stan@gouvernon.org>
2023-10-10 11:18:16 +0200

    Labo 3.

---
+++ Labo3_Start/Sources/main.asm
@@ -0,0 +1,67 @@
+;------------------------------------------------------------
+;Copyright 2003-20xx Haute école ARC Ingéniérie, Switzerland. 
+;All rights reserved.
+;------------------------------------------------------------
+;Nom du fichier :	main.asm
+;Auteur et Date :	Monnerat Serge 27.10.2020
+
+;But : Laboratoire 2
+
+;Modifications
+;Date		Faite	Ctrl		Description
+;
+;------------------------------------------------------------
+
+;--------------------------------------------------------------
+; Exportation des symboles
+;--------------------------------------------------------------
+            XDEF Entry,_Startup,main
+
+;--------------------------------------------------------------
+; Déclaration des constantes d'assemblage
+;--------------------------------------------------------------
+; Vos déclarations de constantes d'assemblage ici
+
+
+;--------------------------------------------------------------
+; Section pour les variables et les données --> zone de RAM
+;--------------------------------------------------------------
+DEFAULT_RAM:     SECTION
+
+; Vos déclarations de données ici
+
+ 
+;--------------------------------------------------------------
+; Section pour le code et les constante --> zone de ROM
+;--------------------------------------------------------------
+DEFAULT_ROM:     SECTION
+
+; Vos constantes ici
+
+
+main:
+_Startup:
+Entry:
+        ld X,#$32F0
+        ld D0,(5,X)
+        ld D0,(-16,X)	
+        ld D1,(-32,X)
+        ld D1,(-160,X)
+        ld X,(-260,X)
+        
+        ld D2,(-90,X)
+        ld D2,(99,X)
+        ld Y,(113,X)
+        ld S,(,X)
+        ld S,(-206,X)
+        
+        
+loop:
+		; Votre codes ici
+		
+		
+		
+		
+		
+		
+		BRA loop          		; boucle infinie
