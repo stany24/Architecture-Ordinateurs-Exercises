a07c97a293989389d662f342394af9b4e81bb183
Gouvernon <stan@gouvernon.org>
2023-10-25 11:28:59 +0200

    Began labo 4.

---
+++ Labo4_Start/Sources/main.asm
@@ -0,0 +1,110 @@
+;------------------------------------------------------------
+;Copyright 2003-20xx Haute école ARC Ingéniérie, Switzerland. 
+;All rights reserved.
+;------------------------------------------------------------
+;Nom du fichier :	main.asm
+;Auteur et Date :	Monnerat Serge 27.10.2020
+
+;But : Laboratoire 4
+
+;Modifications
+;Date		Faite	Ctrl		Description
+;
+;------------------------------------------------------------
+
+;--------------------------------------------------------------
+; Exportation des symboles
+;--------------------------------------------------------------
+            XDEF Entry,_Startup,main
+
+;--------------------------------------------------------------
+; Déclaration des constantes d'assemblage
+;--------------------------------------------------------------
+; Vos déclarations de constantes d'assemblage ici
+
+
+
+;--------------------------------------------------------------
+; Section pour les variables et les données --> zone de RAM
+;--------------------------------------------------------------
+DEFAULT_RAM:     SECTION
+
+; Vos déclarations de données ici
+V1:ds.l 1 
+V2:ds.l 1 
+V3:ds.l 1 
+RES:ds.l 2 
+
+N1:ds.w 1
+N2:ds.w 1
+N3:ds.w 1
+N4:ds.b 3
+ 
+;--------------------------------------------------------------
+; Section pour le code et les constante --> zone de ROM
+;--------------------------------------------------------------
+DEFAULT_ROM:     SECTION
+
+; Vos constantes ici
+
+
+_Startup:
+main:
+Entry:
+        bra exercice2_4
+        
+
+exercice2_3:
+	; Vos initialisations ici
+        ld D6,#$AABBCCDD
+        st D6,V1
+        ld D6,#$99887766
+        st D6,V2
+        ld D6,#$FFFFFFFF
+        st D6,V3
+        
+        ;clearing
+        clr D0
+        ld D7,#$00000000
+        st D7,RES
+        st D7,RES+4
+        
+        ;addition
+        add D6,V2
+        adc D0,#0
+        add D6,V3
+        adc D0,#0
+        st D6,RES+4
+        st d0,RES+3
+
+exercice2_4:
+	
+
+
+loop:
+        ; Votre codes ici
+       	ld D2,#$AA
+       	st D2,N1
+       	ld D2,#$AA
+       	st D2,N2
+       	ld D2,#$AA
+       	st D2,N3
+       	
+       	;clearing
+       	ld D0,#$00
+       	st D0,N4
+       	st D0,N4+1
+       	st D0,N4+2
+       	clr D0
+       	
+       	;resolution
+       	ld D2,N1
+       	add D2,N2
+       	ld D0,#0
+       	sub D2,N3
+       	sub D0,#0
+       	st D2,N4+1
+       	st D0,N4
+        
+        
+		BRA loop     	; boucle infinie
